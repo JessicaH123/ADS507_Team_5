88da4f7d6bb157daa3e2e9c59865b78d7f50d932
dsklnr <101298501+dsklnr@users.noreply.github.com>
2024-02-18 12:56:25 -0700

    Update ADS 507 ETL Pipeline.py
    Added comments for Logan

--- ADS 507 ETL Pipeline.py
+++ ADS 507 ETL Pipeline.py
@@ -10,15 +10,16 @@ import pymysql as mysql
 
 # Constant values
 # Also config values to access the mysql database
-# TODO figure out a secure way to store user credentials
-HOST_NAME = "<Host NAME>"
+# TODO figure out a secure way to store user credentials (this is a nice to have, but not necessary for the course)
+HOST_NAME = "<HOST NAME>"
 PORT_NUMBER = "<PORT NUMBER>"
 USERNAME = "<USERNAME>"
 PASSWORD = "<PASSWORD>"
 DATABASE = "<DATABASE>"
 
 # Transform the Data
-#TODO This section will need to be performed in SQL, not python
+# TODO 
+# This section will need to be performed in SQL, not python
 def check_if_valid_data(df: pd.DataFrame) -> bool:
     # Check if the dataframe is empty
     if df.empty:
@@ -43,35 +44,30 @@ if __name__ == "__main__":
 
     # Convert the csv data to a structured dataframe
 
-    #TODO
+    # TODO
     # Similaryly to the above code, we need to pull data from csv files
     # This will be much easier than pulling data from an API
     # pd.read_csv() -> parse for desired data/timeframe -> clean data (this is the transform step)
 
-# Validate the Data
-    if check_if_valid_data(df):
-        print("Data valid, proceed to load stage")
-
-    # Load the Data
-    # maybe use sqlalchemy to store data
-    engine = sqlalchemy.create_engine("sqlite:///<DATASET>.sqlite")
-    conn = sqlite3.connect('<FILENAME>.sqlite')
-    cursor = conn.cursor()
-
+    # TODO
     # Create a connection as we have done in the course so far
-    #conn = mysql.connect(host="usd-ads-507.mysql.database.azure.com",port=int(3306),user="student",passwd="ads507password",db='tpch')
+    # conn = mysql.connect(host="usd-ads-507.mysql.database.azure.com",port=int(3306),user="student",passwd="ads507password",db='tpch')
 
     # Create tables with DDL
-    #TODO modify the below table for our database
+    #TODO FOR LOGAN modify the below table for our database
     sql = """
     CREATE TABLE IF NOT EXISTS <TABLE NAME>(
         <COLUMN> VARCHAR(200),
         <COLUMN> VARCHAR(200),
         <COLUMN> VARCHAR(200),
         <COLUMN> VARCHAR(200),
-        <COLUMN> primary_key_constraint PRIMARY KEY (played_at)
+        <COLUMN> primary_key_constraint PRIMARY KEY (<COLUMN>)
     )
     """
+
+    # Validate the Data (transform the data). Since this function needs to do reworked using MySQL, we should call this function after creating the MySQL tables
+    if check_if_valid_data(df):
+        print("Data valid, proceed to creating the database")
 
     # Execute the sql query and create the table above
     cursor.execute(sql)
@@ -79,6 +75,14 @@ if __name__ == "__main__":
 
 
     try:
+        # TABLE NAME should be replaced with the name of the table we are trying to create from the DDL above. ( CREATE TABLE IF NOT EXISTS 
+        # <TABLE NAME>(
+        # <COLUMN> VARCHAR(200),
+        # <COLUMN> VARCHAR(200),
+        # <COLUMN> VARCHAR(200),
+        # <COLUMN> VARCHAR(200),
+        # <COLUMN> primary_key_constraint PRIMARY KEY (<COLUMN>))
+        
         df.to_sql("<TABLE NAME>", con=engine,
                        index=False, if_exists="append")
         print("Data loaded into database")
