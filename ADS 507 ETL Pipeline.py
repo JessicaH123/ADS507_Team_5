864e52ed38819acba7d7602549fd8bc92c6a48ce
dsklnr <101298501+dsklnr@users.noreply.github.com>
2024-02-23 12:52:11 -0700

    Update ADS 507 ETL Pipeline.py
    Updated with DDL and reworked transformations and injection.

--- ADS 507 ETL Pipeline.py
+++ ADS 507 ETL Pipeline.py
@@ -3,39 +3,24 @@ import pandas as pd
 from sqlalchemy.orm import sessionmaker
 import requests
 import json
-from datetime import datetime
+from datetime import datetime, date
 import datetime
 import sqlite3
 import pymysql as mysql
 
-# Constant values
-# Also config values to access the mysql database
-# TODO figure out a secure way to store user credentials (this is a nice to have, but not necessary for the course)
-HOST_NAME = "<HOST NAME>"
-PORT_NUMBER = "<PORT NUMBER>"
-USERNAME = "<USERNAME>"
-PASSWORD = "<PASSWORD>"
-DATABASE = "<DATABASE>"
+# Constant values/database information
+PORT = 3306
+HOST_NAME = "127.0.0.1"
+USERNAME = "root"
+PASSWORD = "password"
+DATABASE = "ads507"
 
-# Transform the Data
-# TODO 
-# This section will need to be performed in SQL, not python
-def check_if_valid_data(df: pd.DataFrame) -> bool:
+# Validate the Data
+def check_if_empty(df: pd.DataFrame, primaryKey) -> bool:
     # Check if the dataframe is empty
     if df.empty:
         print("No information in the dataframe")
         return False
-
-    # Primary Key Check
-    if pd.Series(df["<PRIMARY KEY>"]).is_unique:
-        pass
-    else:
-        raise Exception("Primary Key Check has been violated")
-
-    # Check for null values
-    if df.isnull().values.any():
-        raise Exception("A null value was found")
-
     return True
 
 
@@ -44,22 +29,43 @@ if __name__ == "__main__":
 
     # Convert the csv data to a structured dataframe
 
-    # TODO
-    # Similaryly to the above code, we need to pull data from csv files
-    # This will be much easier than pulling data from an API
-    # pd.read_csv() -> parse for desired data/timeframe -> clean data (this is the transform step)
+    # Extract the raw data
+    collision_data = pd.read_csv(r'C:/Users/benog/OneDrive/Documents/ads507/nyc_collisions/nyc_collisions/database.csv')#, parse_dates=['DATE'])
+    uber_data = pd.read_csv(r'C:/Users/benog/OneDrive/Documents/ads507/Uber_Trips_NYC_2016.csv')#, parse_dates=['Pickup Start Date', 'Pickup End Date'])
+    weather_data = pd.read_csv(r'C:/Users/benog/OneDrive/Documents/ads507/NYC_Central_Park_weather_1869-2022.csv')#, parse_dates=['DATE'])
 
-    # TODO
-    # Create a connection as we have done in the course so far
-    # conn = mysql.connect(host="usd-ads-507.mysql.database.azure.com",port=int(3306),user="student",passwd="ads507password",db='tpch')
+    # Changing the spaces in the names to underscores for use in sql statements
+    collision_data.columns = collision_data.columns.str.replace(' ', '_')
+    uber_data.columns = uber_data.columns.str.replace(' ', '_')
+    weather_data.columns = weather_data.columns.str.replace(' ', '_')
+
+    # Filtering out only the 2016 year
+    #collision_df= collision_data[(collision_dat['DATE'].dt.year == 2016)].reset_index(drop = True)
+    #uber_df = uber_data[(uber_data['Pickup_Start_Date'].dt.year == 2016) & (uber_dat['Pickup_Start_Date'].dt.year == 2016)].reset_index(drop = True)
+    #weather_df= weather_data[(weather_data['DATE'].dt.year == 2016)].reset_index(drop = True)
+
+    # Create a database connection
+    conn = mysql.connect(host=HOST_NAME,port=PORT,user=USERNAME,passwd=PASSWORD,db=DATABASE)
+    cursor = conn.cursor()
 
     # Create tables with DDL
-    #TODO FOR LOGAN modify the below table for our database
-    sql = """
-    CREATE TABLE IF NOT EXISTS <Collisions>(
-        `uniq_key` INT NULL DEFAULT NULL,
-        `date` DATE NULL DEFAULT NULL,
-        `time` TEXT NULL DEFAULT NULL,
+    ddl = """
+    CREATE TABLE IF NOT EXISTS `Daily_Weather` (
+        `day_id` INT PRIMARY KEY AUTO_INCREMENT,
+        `date` DATE NOT NULL,
+        `precip` DOUBLE NOT NULL,
+        `snow_fall` DOUBLE NOT NULL,
+        `snow_depth` DOUBLE NULL DEFAULT NULL,
+        `tmin` INT NOT NULL,
+        `tmax` INT NOT NULL
+    );
+
+    CREATE TABLE IF NOT EXISTS `Collisions`(
+        `collision_id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+        `day_id` INT NOT NULL,
+        `uniq_key` INT NOT NULL,
+        `date` DATE NOT NULL,
+        `time` TEXT NOT NULL,
         `borough` VARCHAR(45) NULL DEFAULT NULL,
         `zip_code` VARCHAR(5) NULL DEFAULT NULL,
         `lat` TEXT NULL DEFAULT NULL,
@@ -68,14 +74,14 @@ if __name__ == "__main__":
         `on_street_name` VARCHAR(60) NULL DEFAULT NULL,
         `cross_street_name` VARCHAR(60) NULL DEFAULT NULL,
         `off_street_name` VARCHAR(60) NULL DEFAULT NULL,
-        `persons_injured` INT NULL DEFAULT NULL,
-        `persons_killed` INT NULL DEFAULT NULL,
-        `peds_injured` INT NULL DEFAULT NULL,
-        `peds_killed` INT NULL DEFAULT NULL,
-        `cyclists_injured` INT NULL DEFAULT NULL,
-        `cyclists_killed` INT NULL DEFAULT NULL,
-        `motorists_injured` INT NULL DEFAULT NULL,
-        `motorists_killed` INT NULL DEFAULT NULL,
+        `persons_injured` INT NOT NULL,
+        `persons_killed` INT NOT NULL,
+        `peds_injured` INT NOT NULL,
+        `peds_killed` INT NOT NULL,
+        `cyclists_injured` INT NOT NULL,
+        `cyclists_killed` INT NOT NULL,
+        `motorists_injured` INT NOT NULL,
+        `motorists_killed` INT NOT NULL,
         `vehicle_1_type` VARCHAR(60) NULL DEFAULT NULL,
         `vehicle_2_type` VARCHAR(60) NULL DEFAULT NULL,
         `vehicle_3_type` VARCHAR(60) NULL DEFAULT NULL,
@@ -85,51 +91,67 @@ if __name__ == "__main__":
         `vehicle_2_factor` VARCHAR(60) NULL DEFAULT NULL,
         `vehicle_3_factor` VARCHAR(60) NULL DEFAULT NULL,
         `vehicle_4_factor` VARCHAR(60) NULL DEFAULT NULL,
-        `vehicle_5_factor` VARCHAR(60) NULL DEFAULT NULL)
+        `vehicle_5_factor` VARCHAR(60) NULL DEFAULT NULL,
+        FOREIGN KEY (`day_id`) REFERENCES `Daily_Weather`(`day_id`)
+    );
 
-    CREATE TABLE IF NOT EXISTS <Uber>(
-        `base_license_num` VARCHAR(6) NULL DEFAULT NULL,
-        `wave_num` INT NULL DEFAULT NULL,
-        `base_name` VARCHAR(45) NULL DEFAULT NULL,
-        `dba` VARCHAR(45) NULL DEFAULT NULL,
-        `years` INT NULL DEFAULT NULL,
-        `week_num` INT NULL DEFAULT NULL,
-        `pickup_start_date` DATE NULL DEFAULT NULL,
-        `pickup_end_date` DATE NULL DEFAULT NULL,
-        `total_dispatched_trips` INT NULL DEFAULT NULL,
-        `unq_dispatched_vehicle` INT NULL DEFAULT NULL)
-
-    CREATE TABLE IF NOT EXISTS <Weather>
-        `date` DATE NULL DEFAULT NULL,
-        `precip` DOUBLE NULL DEFAULT NULL,
-        `snow_fall` DOUBLE NULL DEFAULT NULL,
-        `snow_depth` DOUBLE NULL DEFAULT NULL,
-        `tmin` INT NULL DEFAULT NULL,
-        `tmax` INT NULL DEFAULT NULL,
-    UNIQUE INDEX `DATE_UNIQUE` (`date` ASC))
+    CREATE TABLE IF NOT EXISTS `Uber` (
+        `ride_id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+        `day_id` INT NOT NULL,
+        `collision_id` INT NOT NULL,
+        `base_license_num` VARCHAR(7) NOT NULL,
+        `wave_num` INT NOT NULL,
+        `base_name` VARCHAR(45) NOT NULL,
+        `dba` VARCHAR(45) NOT NULL,
+        `years` INT NOT NULL,
+        `week_num` INT NOT NULL,
+        `pickup_start_date` DATE NOT NULL,
+        `pickup_end_date` DATE NOT NULL,
+        `total_dispatched_trips` INT NOT NULL,
+        `unq_dispatched_vehicle` INT NOT NULL,
+        FOREIGN KEY (`day_id`) REFERENCES `Daily_Weather`(`day_id`)
+    );
     """
 
-    # Validate the Data (transform the data). Since this function needs to do reworked using MySQL, we should call this function after creating the MySQL tables
-    if check_if_valid_data(df):
-        print("Data valid, proceed to creating the database")
+    # Validate the data sets
+    if check_if_empty(weather_data, "date"):
+        print("Weather data valid, checking Collision data")
+    if check_if_empty(collision_data, "uniq_key"):
+        print("Collision data valid, checking Uber data")
+    if check_if_empty(uber_data, "collision_id"):
+        print("Uber data valid, proceed to creating the database")
 
     # Execute the sql query and create the table above
-    cursor.execute(sql)
+    cursor.execute(ddl)
     print("Successfully created the database")
 
 
-    try:
-        # TABLE NAME should be replaced with the name of the table we are trying to create from the DDL above. ( CREATE TABLE IF NOT EXISTS 
-        # <TABLE NAME>(
-        # <COLUMN> VARCHAR(200),
-        # <COLUMN> VARCHAR(200),
-        # <COLUMN> VARCHAR(200),
-        # <COLUMN> VARCHAR(200),
-        # <COLUMN> primary_key_constraint PRIMARY KEY (<COLUMN>))
+    try:    
+        # Load the dataframes into the database    
+        weather_data.to_sql("Daily_Weather", conn, if_exists="append", index=False)
+        collision_data.to_sql("Collisions", conn, if_exists="append", index=False)
+        uber_data.to_sql("Uber", conn, if_exists="append", index=False)
         
-        df.to_sql("<TABLE NAME>", con=engine,
-                       index=False, if_exists="append")
-        print("Data loaded into database")
+        # Filtering out only the 2016 year
+        transform_collision = """
+        SELECT *
+        FROM Collisions
+        WHERE YEAR(`DATE`) = 2016;
+        """
+
+        transform_uber = """
+        SELECT *
+        FROM Uber
+        WHERE YEAR(`Pickup_Start_Date`) = 2016;
+        """
+
+        transform_weather = """
+        SELECT *
+        FROM Daily_Weather
+        WHERE YEAR(`DATE`) = 2016;
+        """
+        print("Data was tranformed to show columns from 2016")
+        
     except:
         print("Data already exists in the database")
 
